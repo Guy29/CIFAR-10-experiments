b630a5350310c1a5c9c7a47b19b10b50e7fc0056
Guy <ga327@sussex.ac.uk>
2024-05-09 01:22:58 +0100

    Create gitignore

--- test.py
+++ test.py
@@ -16,16 +16,16 @@ class NeuralNetwork(nn.Module):
         
         self.conv_layers = nn.Sequential(
             nn.Conv2d( 3,  32, kernel_size=3, padding=1),
-            nn.ReLU(),
             nn.MaxPool2d(2, 2),
+            nn.ReLU(),
             
             nn.Conv2d(32,  64, kernel_size=3, padding=1),
-            nn.ReLU(),
             nn.MaxPool2d(2, 2),
+            nn.ReLU(),
             
             nn.Conv2d(64, 128, kernel_size=3, padding=1),
-            nn.ReLU(),
             nn.MaxPool2d(2, 2),
+            nn.ReLU(),
         )
         
         self.flatten = nn.Flatten()
